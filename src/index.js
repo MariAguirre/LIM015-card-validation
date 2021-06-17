9f9080bac90e9b045e21ea4bf4444428baf3769b
MariAguirre <75194098+MariAguirre@users.noreply.github.com>
2021-06-17 08:41:30 -0500

    subir proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
-import validator from './validator.js';
 
-console.log(validator);
+import validator from "./validator.js";
+
+/*----------------declaración de variables-------------------*/
+let btnform = document.getElementById('btnform'),
+    btnvalidate = document.getElementById('btnvalidate'),
+    btnpago = document.getElementById('btnpago'),
+    btnregreso = document.getElementById('btnregreso'),
+    numbercard = document.getElementById('numbercard');
+
+/*----------------Ocultar y mostrar secciones----------------*/
+//evento para mostrar el formulario
+btnform.addEventListener("click", () => {
+    document.getElementById('form').style.display = "block";
+    document.getElementById('inicio').style.display = "none";
+
+});
+//Evento para regresar al inicio
+btnregreso.addEventListener("click", () => {
+    document.getElementById('inicio').style.display = "block";
+    document.getElementById('final').style.display = "none";
+});
+//Evento para regresar al inicio
+btnpago.addEventListener("click", () => {
+    alert("!Se realizo su pago con exito!");
+
+});
+/*---------------------- Operaciones ------------------------*/
+//validar solo numerosq
+numbercard.addEventListener('keyup', (e) => {
+    let card = e.target.value;
+    numbercard.value = card
+        //reemplaza los espacios en blanco por vacio
+        .replace(/\s/g, '')
+        //reemplaza las letras por vacio
+        .replace(/\D/g, '')
+        //busca los numeros del 0-9, los separa por 4 digitos
+        //y muestra los 4 digitos mas un espacio en blanco
+        .replace(/([0-9])/g, '$1')
+        //elimina los espacios en blanco
+        .trim();
+
+
+});
+//Evento para validar y enmascarar los numeros de la tarjeta
+//Envia datos de la validación a la siguientes seccion 
+btnvalidate.addEventListener("click", () => {
+    let creditCardNumber = document.getElementById('numbercard').value;// inicializamos a numbercard el valor del input
+    //
+    if (creditCardNumber) {
+        let isValid = validator.isValid(creditCardNumber);
+        let maskify = validator.maskify(creditCardNumber);
+        document.getElementById("tarjeta").innerHTML = maskify;
+
+        if (isValid) {
+            let array = numbercard.value.split("");
+            let find = array.slice(0, 1);
+            if (find == "4")
+                document.getElementById("cate").innerHTML = "VISA";
+            else if (find == "5")
+                document.getElementById("cate").innerHTML = "MASTERCARD";
+            else
+                document.getElementById("cate").innerHTML = "OTRA CATEGORIA";
+            document.getElementById("validacion").innerHTML = "tu tarjeta es VALIDA";
+        } else {
+            document.getElementById("validacion").innerHTML = "tu tarjeta es INVALIDA";
+        }
+        document.getElementById('final').style.display = "block";
+        document.getElementById("form").style.display = "none";
+
+    } else {
+        alert("Ingresa tu numero de cuenta")
+    }
+    
+
+});
+
+
+
+
+//console.log(validator);
+
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
 const validator = {
-  // ...
+  //...
+  //creditCardNumber :document.getElementById("numbercard").value,
+
+  isValid(creditCardNumber) {
+    let array = creditCardNumber.split("");
+    const arrayInverse = array.reverse();
+    var sum = 0;
+    for (let i = 0; i < arrayInverse.length; i++) {
+      let numero = arrayInverse[i];
+      if ((i % 2) != 0) {//[2 , 6]
+        numero = parseInt(arrayInverse[i]) * 2;
+        if (numero > 9) {//12
+          let impar = numero.toString().split("");//[1 , 2]
+          let sumaimpar = 0;
+          impar.forEach(element => {//recorriendo el miniarray [1, 2]
+            sumaimpar = sumaimpar + parseInt(element); // sumando ambos digitos
+
+          });
+          numero = sumaimpar;// almanenado la suma del miniarray
+        }
+        sum = sum + numero;
+      } else {
+        sum = sum + parseInt(arrayInverse[i]);
+      }
+
+    }
+    let result = sum % 10;
+    if (result == 0)
+      return true;
+    else
+      return false;
+
+  },
+
+  maskify(creditCardNumber) {
+    let hiddendigit = "";
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i < creditCardNumber.length - 4) {
+        //hiddendigit.value = hiddendigit.replace(/\s/g, '#'); 
+        hiddendigit = hiddendigit + "#";
+      } else {
+        hiddendigit = hiddendigit + creditCardNumber[i];
+      }
+    } return hiddendigit;
+
+  }
+
+
 };
+
+
 
 export default validator;
